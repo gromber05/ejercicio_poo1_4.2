c9b4c2ec9d33500831e1c75a65a479054938916e
gromber05 <gromber2911@g.educaand.es>
2025-01-31 15:44:38 +0100

    Actualizado un método de la clase Persona

--- src/main.kt
+++ src/main.kt
@@ -4,8 +4,7 @@ import kotlin.math.pow
 class Persona(var peso: Double, var altura: Double) {
     var nombre: String = ""
 
-    private var imc: Double = 0.0
-        get() = round((peso / altura.pow(2)) * 100) / 100
+    private var imc: Double = round((peso / altura.pow(2)) * 100) / 100
 
     constructor(nombre: String, peso: Double, altura: Double) : this(peso, altura) {
         this.nombre = nombre
